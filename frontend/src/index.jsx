085ced8de7cb256172c4dc4eb49d4049943cfcb9
NairbN <92410537+NairbN@users.noreply.github.com>
2025-08-08 20:50:10 -0700

    EsLint and Prettier for backend

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello, Aux Wars!</h1>
+  return <h1 className="text-3xl font-bold underline">Hello, Aux Wars!</h1>;
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
-
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
