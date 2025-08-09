a0ce570ccbf5f91515e73b19f36b6f9f3960d2ee
NairbN <92410537+NairbN@users.noreply.github.com>
2025-08-08 19:04:35 -0700

    Setup frontend with React, Vite, and Tailwind

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,1 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+function App() {
+  return <h1 className="text-3xl font-bold underline">Hello, Aux Wars!</h1>
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 
